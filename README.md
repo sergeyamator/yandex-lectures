d6301fc6e1192322f5c9e41394c19005ed7f9f56
Sergey Rubets <srubets@speroteck.com>
2017-04-03 11:01:58 +0300

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+Open Terminal (for Mac and Linux users) or the command prompt (for Windows users)
+
+1. Clone this repo
+2. run "npm install"
+3. run "npm start"
---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
+const rename = require("gulp-rename");
+const plumber = require('gulp-plumber');
+const runSequence = require('run-sequence');
+const clean = require('gulp-clean');
+
+/* --------- paths --------- */
+var paths = {
+  scss: {
+    location: [
+      'source/scss/main.scss'
+    ],
+    destination: 'prod/css'
+  },
+
+  js: {
+    location: [
+      'source/js/modules/init.js'
+    ],
+    destination: 'prod/js'
+  }
+};
+
+/* ----- pug ----- */
+gulp.task('pug-compile', function() {
+  gulp.src('source/pug/index.pug')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('prod/'))
+});
+
+/* ------ sass ------ */
+gulp.task('sass-compile', function() {
+  gulp.src(paths.scss.location)
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(rename('main.min.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.destination));
+});
+
+/* -------- concat js -------- */
+gulp.task('concat-js', function() {
+  return gulp.src(paths.js.location)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.js.destination));
+});
+
+/* -------- Assets clean ------- */
+gulp.task('assets-clean', function() {
+  return gulp.src('prod/assets', {read: false})
+    .pipe(clean());
+});
+
+/* ---------- Assets copy ---------- */
+gulp.task('assets-copy', function() {
+  return gulp.src('source/assets/**/*')
+    .pipe(gulp.dest('prod/assets'));
+});
+
+gulp.task('assets', function() {
+  runSequence('assets-clean',
+    'assets-copy');
+});
+
+/* -------- gulp server  -------- */
+gulp.task('server', function() {
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'prod'
+    }
+  });
+});
+
+/* -------- gulp watching  -------- */
+gulp.task('watch', function() {
+  gulp.watch('source/pug/**/*.pug', ['pug-compile']);
+  gulp.watch('source/scss/**/*.scss', ['sass-compile']);
+  gulp.watch('source/js/modules/*.js', ['concat-js']);
+  gulp.watch('source/assets/**/*', ['assets']);
+  gulp.watch([
+    'prod/**/*.html',
+    'prod/js/**/*.js',
+    'prod/css/**/*.css'
+  ]).on('change', browserSync.reload);
+});
+
+gulp.task('default', function() {
+  runSequence(
+    'pug-compile',
+    'sass-compile',
+    'concat-js',
+    'assets',
+    'server',
+    'watch')
+});
+
+
